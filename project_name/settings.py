0d491e32c6bf8201cc975b55f0bd6b538d4996ae
scott <scott@cogolabs.com>
2017-03-11 14:19:48 -0500

    add auth user settings, localhost to debug database

--- project_name/settings.py
+++ project_name/settings.py
@@ -78,12 +78,18 @@ WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '{{ project_name }}',
+        'HOST': 'localhost',
+        'PORT': '5432'},
     }
-}
+else:
+    DATABASES = {'default': dj_database_url.config()}
+
+
+AUTH_USER_MODEL = '{{ project_name }}.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
